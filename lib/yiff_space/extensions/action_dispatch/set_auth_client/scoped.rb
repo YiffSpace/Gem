658a04e6fc9b60176d049f384414dbf0a6412dda
Donovan_DMC <hewwo@yiff.rocks>
2026-04-29 10:42:51 -0500

    moved scoped ext

---
+++ lib/yiff_space/extensions/action_dispatch/set_auth_client/scoped.rb
@@ -0,0 +1,13 @@
+module YiffSpace
+  module Extensions
+    module ActionDispatch
+      module SetAuthClient
+        module Scoped
+          def yiffspace_set_auth_client(name, &)
+            constraints(Auth::SetClientName.new(name), &)
+          end
+        end
+      end
+    end
+  end
+end
