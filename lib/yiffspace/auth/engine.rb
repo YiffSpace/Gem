06d3a631e9bd8597f027387ad2e7ebfee3a6c36d
Donovan_DMC <hewwo@yiff.rocks>
2026-06-22 02:34:50 -0500

    Fix webhook action

--- lib/yiffspace/auth/engine.rb
+++ lib/yiffspace/auth/engine.rb
@@ -41,7 +41,7 @@ module YiffSpace
             subclass.routes.default_scope = { module: "yiff_space/auth" }
             subclass.routes.draw do
               constraints(SetClientName.new(name)) do
-                post(:webhook, controller: :webhook)
+                post(:webhook, controller: :webhook, action: :create)
                 get(:cb, controller: :root)
                 get(:logout, controller: :root)
                 get(:permissions, controller: :root)
